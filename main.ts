909ee2420053ad0bab1f1c6d8c03d7fc7ddddde5
Azlankhan12345 <161835064+Azlankhan12345@users.noreply.github.com>
2024-05-02 16:24:04 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,63 +1,76 @@
-#!/usr/bin/env node
-
+#! /usr/bin/env node
 import inquirer from "inquirer";
-import chalk from "chalk";
 
-class Student {
+class student {
     name: string;
-    constructor(name: string) {
-        this.name = name;
-    };
-};
+    constructor(n: string) {
+        this.name = n
+    }
+}
+class friend {
+    nam : string;
+    constructor(z: string) {
+        this.nam = z
+    }
+}
+class person {
+    students: student[] = []
+    friends: friend[] = []
 
-class Person {
-    students: Student[] = [];
+    addStd(obj: student) {
+        this.students.push(obj)
+    }
+    addfri(fd: friend) {
+        this.friends.push(fd)
+    }
+}
+let outCome = new person()
 
-    addStudent(obj: Student) {
-        this.students.push(obj);
-    };
-};
-
-const person = new Person();
-
-const programStart = async (person: Person) => {
-    console.log(chalk.bold.green("welcome guest"));
-    do {
-
-        const ans = await inquirer.prompt({
-            type: "list",
-            name: "select",
-            message: "which person do you want to talk?",
-            choices: ["Azlan", "Student"]
-        });
-
-        if (ans.select === "Azlan") {
-            console.log(`You are chatting with ${chalk.bold.green("Azlan")}`);
-            console.log("Hope you are doing well!");
+const start = async (per: person) => {
+    console.log("You are welcome");
+    do{
+    const ans = await inquirer.prompt({
+        type: "list",
+        name: "an",
+        message: "Who would you like to talk ?",
+        choices: ["Friends", "Students"]
+    })
+    if(ans.an == "Friends") {
+        let fri = await inquirer.prompt({
+            type: "input",
+            name: "f",
+            message: "By which friend do you want to talk ?"
+        })
+        const fri1 = per.friends.find(x => x.nam == fri.f)
+        if(!fri1) {
+            const ff = new friend(fri.f) 
+            per.addfri(ff)
+            console.log(`Hello i am ${ff.nam}`)
+            console.log(per.friends)
         }
-        if (ans.select == "Student") {
-            const ans = await inquirer.prompt({
-                type: "input",
-                name: "student",
-                message: "Which student do you want to talk?"
-            })
-
-            const student = person.students.find((val) => val.name == ans.student);
-
-            if (!student) {
-                const name = new Student(ans.student);
-                person.addStudent(name);
-
-                console.log(`I am ${chalk.bold.green(name.name)}, and I am fine.`);
-                console.log(person.students);
-            }
-            if (student) {
-
-                console.log(`I am ${chalk.bold.green(student.name)}, and I am fine.`);
-                console.log(person.students);
-            };
-        };
-    } while (true);
-};
-
-programStart(person);
+        if(fri1) {
+            console.log(`Hello i am your ${fri1.nam}`)
+            console.log(per.friends)
+        }
+    }
+    if(ans.an == "Students") {
+        let std = await inquirer.prompt({
+            type: "input",
+            name: "s",
+            message: "By which student do you want to talk ?"
+        })
+        const std1 = per.students.find(val => val.name == std.s)
+        if(!std1) {
+            const n = new student(std.s)
+            per.addStd(n)
+            console.log(`Hello sir, i am your student ${n.name}`)
+            console.log(per.students)
+        }
+        if(std1) {
+            console.log(`Hello sir, i am your student ${std1.name}`)
+            console.log(per.students)
+        }
+    }
+}while(true)
+}
+start(outCome)
